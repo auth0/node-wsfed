414958ef0ee5d48d7f823517976493a2b237d2f3
Gustavo Narea <gustavo.narea@auth0.com>
2018-02-27 15:23:02 +0000

    Revert "Make compatible with Node 0.10 🤢"
    This reverts commit eaf1ad7

--- test/encoders.tests.js
+++ test/encoders.tests.js
@@ -1,11 +1,13 @@
-var assert = require('assert');
-var encoders = require('../lib/encoders');
-var fixtures = require('./fixture/server');
+'use strict';
+
+const assert = require('assert');
+const encoders = require('../lib/encoders');
+const fixtures = require('./fixture/server');
 
 describe('encoders', function () {
   describe('thumbprint', function () {
     it('should return the thumbprint in all caps', function () {
-      var certThumbprint = encoders.thumbprint(fixtures.credentials.cert);
+      const certThumbprint = encoders.thumbprint(fixtures.credentials.cert);
       assert.equal(certThumbprint, '499FDF1C2218A99C8595AAC2FD95CE36F0A6D59D');
     });
   });
