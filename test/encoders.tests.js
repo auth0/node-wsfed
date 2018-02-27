eaf1ad7298f958981e1c57f59a407d652333f951
Gustavo Narea <gustavo.narea@auth0.com>
2018-02-27 11:42:18 +0000

    Make compatible with Node 0.10 🤢

--- test/encoders.tests.js
+++ test/encoders.tests.js
@@ -1,14 +1,12 @@
-'use strict';
-
-const assert = require('assert');
-const encoders = require('../lib/encoders');
-const fixtures = require('./fixture/server');
+var assert = require('assert');
+var encoders = require('../lib/encoders');
+var fixtures = require('./fixture/server');
 
 
 describe('encoders', function () {
   describe('thumbprint', function () {
     it('should return the thumbprint in all caps', function () {
-      const certThumbprint = encoders.thumbprint(fixtures.credentials.cert);
+      var certThumbprint = encoders.thumbprint(fixtures.credentials.cert);
       assert.equal(certThumbprint, '499FDF1C2218A99C8595AAC2FD95CE36F0A6D59D');
     });
   });
